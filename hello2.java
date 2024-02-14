3c6fd9eb3912e2274eaee0022117c49e63010514
prathvik0 <159225681+prathvik0@users.noreply.github.com>
2024-02-13 20:30:33 -0800

    Create hello2.java

---
+++ hello2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class hello2 {
+{
+public static void main(String args[]) {
+
+
+
+        System.ot.println("enter username");
+           Scanner myobj=new Scanner(System.in);
+        String username;
+        username=myobj.nextline();
+
+
+
+System.out.println("username is:"+username);
+}
+}
